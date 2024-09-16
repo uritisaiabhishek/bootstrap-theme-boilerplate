54d9b13a61d694e59fb43575f097e88287035d4a
uritisaiabhishek <uritisaiabhishek@gmail.com>
2024-09-16 13:26:03 +0530

    initial partials setup added hrader in partial added footer in partial

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,17 @@ const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
+const fileInclude = require('gulp-file-include');
 
 // Paths
 const paths = {
     scss: './src/assets/scss/**/*.scss',
     js: './src/assets/js/**/*.js',
     html: './src/**/*.html',
-    images: './src/assets/images/**/*',  // Path for images
+    images: './src/assets/images/**/*',
+    includes: './includes/**/*.html',  // Include header.html
     dist: './build/',
-    bootstrapJS: './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-    popperJS: './node_modules/@popperjs/core/dist/umd/popper.min.js'
 };
-
-// Copy images to build folder
-function images() {
-    return gulp.src(paths.images)
-        .pipe(gulp.dest(`${paths.dist}assets/images`));
-}
 
 // Compile SCSS to CSS
 function styles() {
@@ -39,19 +33,29 @@ function styles() {
 
 // Minify JS and include Bootstrap and Popper.js
 function scripts() {
-    return gulp.src([paths.js, paths.popperJS, paths.bootstrapJS])
+    return gulp.src([paths.js, './node_modules/@popperjs/core/dist/umd/popper.min.js', './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'])
         .pipe(sourcemaps.init())
-        .pipe(concat('main.min.js'))  // Concatenate all JS files
+        .pipe(concat('main.min.js'))
         .pipe(terser())
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(`${paths.dist}assets/js`));
 }
 
-// Minify HTML
+// Minify and include partials (header.html)
 function html() {
     return gulp.src(paths.html)
+        .pipe(fileInclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest(paths.dist));
+}
+
+// Copy images to build folder
+function images() {
+    return gulp.src(paths.images)
+        .pipe(gulp.dest(`${paths.dist}assets/images`));
 }
 
 // Watch for changes
@@ -65,12 +69,14 @@ function serve() {
     gulp.watch(paths.js, scripts).on('change', browserSync.reload);
     gulp.watch(paths.html, html).on('change', browserSync.reload);
     gulp.watch(paths.images, images).on('change', browserSync.reload);
+    gulp.watch(paths.includes, html).on('change', browserSync.reload);  // Watch for changes in includes
 }
 
 // Define tasks
 exports.styles = styles;
 exports.scripts = scripts;
 exports.html = html;
+exports.images = images;
 exports.serve = serve;
 
 // Default task
